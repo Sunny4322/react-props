0542909fe1c440128b58f11c7516b044c5860cee
Sunny4322 <ks3411262@gmail.com>
2022-11-30 12:03:01 +0530

    30-11-22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/*import ClassComponent from './ClassComponent'
+import ClassComponent2 from './ClassComponent2'
+function App(){
+	return(
+		<div>
+			<p> hello welcome</p>
+				<h1>keep smile</h1>
+				<ClassComponent/>
+				<img src="https://i.pinimg.com/originals/9c/b0/70/9cb070d62dc738a0c3a1a408d68e4af5.jpg"/>
+				<ClassComponent2/>
+				<img src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"/>
+			</div>
+		)
+}
+export default App*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PropsExample from './PropsExample'
+function App(){
+	return(
+          <div>
+          <h1>This is about props example</h1>
+          <PropsExample name="Sunny" age="20"/>
+          </div>   
+		)
 }
 
-export default App;
+export default App
